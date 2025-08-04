74caf7e61f9159e3a559f0fc20f6343c3feed83c
yukio <zviamontevilca@gmail.com>
2025-08-04 14:37:19 -0500

    connect axios productos

--- desarrollo web1/ejercicioReact/src/pages/ListProductView.tsx
+++ desarrollo web1/ejercicioReact/src/pages/ListProductView.tsx
@@ -1,13 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../config/axiosConnect";
 
+interface Productos{
+    id:number,
+    nombre:string,
+    descripcion:string,
+    marca:string,
+    precio:number,
+    foto:string,
+    color:string
+
+}
 
 const ListProductView  = ()=>{
-
+    const [productos,setproductos] = useState<Productos[]>([]);
     useEffect(()=>{
         const obtenerProductos = async()=>{
             await api.get("/productos").then(datos=>{
                 console.log(datos.data);
+                setproductos(datos.data.data);
             }).catch((error)=>{
                 console.log(error)
             })
@@ -18,7 +29,17 @@ const ListProductView  = ()=>{
 
     return (
         <div>
-            productm os
+            productos
+            <tbody>
+                { productos.map((producto)=>(
+                    <tr key={producto.id} >
+                        <td> { producto.nombre } </td>
+                        <td> {producto.descripcion} </td>
+                        <td> {producto.color } </td>
+                        <td> {producto.precio} </td>
+                    </tr>
+                ))}
+            </tbody>
         </div>
     );
 }
